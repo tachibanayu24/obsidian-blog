265248e9c440bae5c0e4a146c9fce424c260502c
Yuto Tachibana <yuto.tachibana@dena.com>
2025-04-10 14:14:59 +0900

    Update

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,10 +17,14 @@ export default (() => {
           <a href="https://x.com/tachibanayu24" target="_blank" rel="noopener noreferrer">tachibanayu24</a> © {year}
         </p>
 
-        <p style={{ fontSize: "0.725rem", lineHeight: "1.2" }}>
-          このブログは、
+        <p style={{ fontSize: "0.725rem", lineHeight: "1.2", margin: "0.25rem" }}>
+          このブログは
           <a href="https://quartz.jzhao.xyz/" target="_blank" rel="noopener noreferrer">Quartz</a>
           をベースに作成しています。<a href="https://policies.google.com/technologies/partner-sites?hl=ja" target="_blank" rel="noopener noreferrer">Google Analytics</a>を使用してアクセス解析を行っています。
+        </p>
+        <p style={{ fontSize: "0.725rem", lineHeight: "1.2", margin: "0.25rem", display: "flex", gap: "0.5rem", justifyContent: "center" }}>
+          <a href="/index.xml" target="_blank" rel="noopener noreferrer">RSSフィード</a>
+          <a href="https://github.com/tachibanayu24/hokori-log" target="_blank" rel="noopener noreferrer">ソースコード</a>
         </p>
       </footer>
     )
